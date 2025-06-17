0a7fc09cd79593b1b45ee3ff3a20f0e8f0d9ec6f
cheran99 <cheransinnathamby@gmail.com>
2025-06-17 12:28:30 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "cloud_run_url" {
   description = "URL of deployed Cloud Run app"
-  value       = google_cloud_run_service.app.status[0].url
+  value       = google_cloud_run_v2_service.app.status[0].url
 }
