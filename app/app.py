eb3ee9adb390740c0e7c11f21eddf79a2568e3a5
cheran99 <cheransinnathamby@gmail.com>
2025-08-11 00:23:58 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template, jsonify, request, session, redirect, url_for
-from google.cloud import secretmanager
+from flask import Flask, render_template, request, redirect, url_for
 from google.cloud.sql.connector import Connector, IPTypes
 import pymysql
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Float, Boolean, select, insert, update, engine, URL
+from sqlalchemy import (create_engine, 
+                        MetaData, 
+                        Table, 
+                        Column, 
+                        Integer, 
+                        String, 
+                        Boolean, 
+                        select, 
+                        insert, 
+                        update, 
+                        engine)
 import os
 
 app = Flask(__name__)
 
 project_id = "devsecops-pipeline-463112"
+
 
 def access_secret_version(secret_id: str):
     from google.cloud import secretmanager
@@ -17,6 +27,7 @@ def access_secret_version(secret_id: str):
     payload = response.payload.data.decode("UTF-8")
     return payload
 
+
 def get_db_connection() -> engine.base.Engine:
     user = access_secret_version("db-user")
     password = access_secret_version("db-pass")
@@ -24,13 +35,15 @@ def get_db_connection() -> engine.base.Engine:
     instance_connection_name = access_secret_version("instance-connection-name")
     port = 3306
 
-    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC 
+    ip_type = IPTypes.PRIVATE if os.environ.get(
+        "PRIVATE_IP"
+        ) else IPTypes.PUBLIC
     connector = Connector(ip_type=ip_type, refresh_strategy="LAZY")
 
     def getconn() -> pymysql.connections.Connection:
         conn: pymysql.connections.Connection = connector.connect(
-            instance_connection_string=instance_connection_name,  
-            driver="pymysql",          
+            instance_connection_string=instance_connection_name,
+            driver="pymysql",
             user=user,
             password=password,
             port=port,
@@ -44,13 +57,15 @@ def get_db_connection() -> engine.base.Engine:
         pool_size=5,
         max_overflow=2,
         pool_timeout=30,
-        pool_recycle=1800,        
+        pool_recycle=1800,
     )
     return engine
 
-engine = get_db_connection () 
+
+engine = get_db_connection()
 metadata = MetaData()
-    
+
+
 todos = Table(
     "todos", metadata,
     Column("id", Integer, primary_key=True, autoincrement=True),
@@ -60,31 +75,40 @@ todos = Table(
 
 metadata.create_all(engine)
 
+
 @app.route('/')
 def index():
     with engine.connect() as conn:
-        incomplete = conn.execute(select(todos).where(todos.c.complete == False)).fetchall()   #todos.query.filter_by(complete=False).all()
-        complete = conn.execute(select(todos).where(todos.c.complete == True)).fetchall() #todos.query.filter_by(complete=True).all()
+        incomplete = conn.execute(
+            select(todos).where(todos.c.complete.is_(False))
+            ).fetchall()
+        complete = conn.execute(
+            select(todos).where(todos.c.complete.is_(True))
+            ).fetchall()
 
         return render_template('index.html', incomplete=incomplete, complete=complete)
+
 
 @app.route('/add', methods=['POST'])
 def add():
     with engine.begin() as conn:
         task = request.form["to-do item"]
-        query = insert(todos).values(text=task, complete = False) 
-        #todo = todos(text=request.form['todoitem'], complete=False)
+        query = insert(todos).values(text=task, complete=False)
         conn.execute(query)
 
         return redirect(url_for('index'))
 
+
 @app.route('/complete/<id>')
 def complete(id):
     with engine.begin() as conn:
-        conn.execute(update(todos).where(todos.c.id == int(id)).values(complete = True)) #todos.query.filter_by(id=int(id)).first()
+        conn.execute(
+            update(todos).where(todos.c.id == int(id)).values(complete=True)
+            )
 
         return redirect(url_for('index'))
-    
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port) #nosec B104  
+    app.run(host="0.0.0.0", port=port) # nosec B104
