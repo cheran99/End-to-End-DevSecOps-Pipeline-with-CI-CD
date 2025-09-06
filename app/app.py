142b83c41bfaa81b398de04053f39a78f1342ba1
cheran99 <cheransinnathamby@gmail.com>
2025-09-06 22:34:31 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 from flask_talisman import Talisman
 from google.cloud.sql.connector import Connector, IPTypes
 import pymysql
